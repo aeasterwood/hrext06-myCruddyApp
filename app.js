f7c416fbf59313880e71deb59a0271e33c7a61d9
Alicia Easterwood <Alicia@Alicias-MacBook-Air.local>
2018-12-20 20:08:34 -0600

    ui delete functionality and styling

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ $(document).ready(function(){
   var inputs = [];
 
 
-  
-
   // write to local storage from input when button save clicked
   $('.btn-submit').on('click', function(){
     var tEntry = $('.text-entry').val()
@@ -18,17 +16,16 @@ $(document).ready(function(){
     //console.log('myItemInStorage', myItemInStorage);
 
   // display the values here
-    $('ul').append("<li><span class='delete'><i class='fa fa-trash' aria-hidden='true'></i></span>" + myItemInStorage + "</li>");
+    $('ul').append("<li><button class='btn-delete' button type='button'><i class='fa fa-trash' aria-hidden='true'></i></button>" + myItemInStorage + "</li>");
     $('.text-entry').val("")
   });
 
-  //strikethrough 
-  $('ul').on('click', 'li', function(){
-    $(this).toggleClass('done')
-  })
+ 
+  $('.list-display-field').on('click', '.btn-delete', function(){
+  //remove the values
+   $(this).parent().remove();
 
-  // delete from local storage when delete button clicked
-  $('.btn-delete').on('click', function(){
+  //delete from local storage when delete button clicked
     localStorage.removeItem('inputFieldValue');
   });
 
